5585ecb65b4b6e082d12518e531abf3b02fcedcc
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-31 21:33:47 -0700

    last commit of the day

--- UCB/Python/Django/app/users/urls.py
+++ UCB/Python/Django/app/users/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.SnippetListView.as_view(), name='index')
-    # url(r'^index/$', views.index, name='index'),
-    url(r'^detail/(?P<user_id>[0-9]+)/$', views.UserDetailView.as_view(), name='detail'),
+    url(r'^index/$', views.UserListView.as_view(), name='index'),
+
+    url(r'^detail/(?P<pk>[0-9]+)/$', 
+        views.UserDetailView.as_view(), name='detail'),
+
     url(r'^add/$', views.add, name='add'),
 ]
-
-# (?P<name>pattern) the "P" here is just python convention
--- UCB/Python/Django/app/users/views.py
+++ UCB/Python/Django/app/users/views.py
@@ -1,38 +1,29 @@
-from django.shortcuts import get_object_or_404, render
-
 from django.http import Http404, HttpResponse
-from django.urls import reverse
 
-from django.views.generic import DetailView, ListView 
+from django.shortcuts import render
+
+from django.views.generic import DetailView, ListView
 
 from .models import User
 
-
 class UserListView(ListView):
-    model = Usertemplate_name = 'users/index.html'
+    model = User
+    template_name = 'users/index.html'
 
     def get_context_data(self, **kwargs):
-        context = super(UserListView, self).get_context_data
-        (**kwargs)
+        context = super(UserListView, self).get_context_data(**kwargs)
 
+        # In real life we'd retrieve this from the session.
         context['name'] = 'Adonis'
-
+        
         return context
 
-
-# passing in the user number
-def detail(request, user_id):
-    try:
-        context =  { 'user' : User.objects.get(id=user_id) }
-    except User.DoesNotExist:
-        raise Http404('User does not exist')
-
-    return render(request, 'users/detail.html', context)
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'users/detail.html'
 
 def add(request):
 
     context = { 'header' : 'This is the add view!'}
 
     return render(request, 'users/add.html', context)
-
-# THIS IS YOUR CONTROLLER
